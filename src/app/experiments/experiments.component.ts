f206c6f99f10e68c9d339504423daa7717a34f62
Andrew Pomerleau <arpomerleau@gmail.com>
2022-06-30 20:47:04 -0400

    Bonus: enabled add, edit and remove functionality for Experiments as well.

--- src/app/experiments/experiments.component.ts
+++ src/app/experiments/experiments.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-experiments',
-  templateUrl: './experiments.component.html',
-  styleUrls: ['./experiments.component.css']
+  selector: "app-experiments",
+  templateUrl: "./experiments.component.html",
+  styleUrls: ["./experiments.component.css"],
 })
 export class ExperimentsComponent implements OnInit {
+  itemBeingEdited: string = "";
 
-  constructor() { }
+  items: string[] = [
+    "Mars soil sample",
+    "Plant growth in habitat",
+    "Human bone density",
+  ];
 
-  ngOnInit() {
+  constructor() {}
+
+  add(name: string) {
+    if (this.items.includes(name)) {
+      alert(`"${name}" is already someone's experiment.`);
+      return false;
+    }
+
+    this.items.push(name);
+    return true;
   }
 
+  remove(item: string) {
+    let index = this.items.indexOf(item);
+    this.items.splice(index, 1);
+  }
+
+  edit(item: string) {
+    this.itemBeingEdited = item;
+  }
+
+  save(name: string, item: string) {
+    let index = this.items.indexOf(item);
+    this.items[index] = name;
+  }
+
+  ngOnInit() {}
 }
