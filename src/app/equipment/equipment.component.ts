46ce0c4de7d346ac5791b259eb471accf333f457
Andrew Pomerleau <arpomerleau@gmail.com>
2022-07-06 20:48:43 -0400

    change add to cargo button colors

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -54,6 +54,14 @@ export class EquipmentComponent implements OnInit {
     return this.maximumAllowedMass - this.cargoMass;
   }
 
+  getButtonColor(item: object): string {
+    if (this.canAdd(item)) {
+      return "khaki";
+    }
+
+    return "";
+  }
+
   getRemainingColor(): string {
     let remaining = this.getMassRemaining();
 
